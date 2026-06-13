c071ae6b0d7fe19a6cc468241719f30b051ceb31
b-beyaz <beyaz.ayse81@gmail.com>
2026-06-13 12:27:29 +0300

    added mysql tag to helm values.yaml file

--- setup/argocd-manifest-setup.sh
+++ setup/argocd-manifest-setup.sh
@@ -10,3 +10,13 @@ if [ $? -ne 0 ]; then
   echo "dropbox-dev namespace oluşmadı, kontrol et"
   exit 1
 fi
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+PF_PID=$!
+sleep 3
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 -d)
+argocd login localhost:8080 --username admin --password $ARGOCD_PASSWORD --insecure
+
+argocd app sync dropbox
+kill $PF_PID
