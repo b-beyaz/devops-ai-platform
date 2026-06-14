34b84f010f94d1d4e2c6dcb5d15d9efdfbaa803f
b-beyaz <beyaz.ayse81@gmail.com>
2026-06-14 23:18:47 +0300

    added image updater

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,5 +8,11 @@ source /mnt/c/devops-ai-platform/target-apps/dropbox-app/.env
 #echo "2. Sealed Secrets controller kurulumu"
 #$SETUP_PATH/sealed-secret-controller-setup.sh
 
-echo "3. ArgoCD manifest apply et"
-$SETUP_PATH/argocd-manifest-setup.sh
+#echo "3. ArgoCD manifest apply et"
+#$SETUP_PATH/argocd-manifest-setup.sh
+
+#echo "4. Sealed secret oluştur ve apply et"
+#$SETUP_PATH/sealed-secret-setup.sh
+
+echo "5. Image Updater"
+$SETUP_PATH/image-updater-setup.sh
